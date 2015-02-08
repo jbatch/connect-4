eaa70944d5f878ceb32ce542ed7e15601ea6836b
Josh Batchelor <josh_batch@live.com.au>
2015-02-08 23:51:00 +0800

    Cleaned up bootstrap files from public

--- connect-4-server.js
+++ connect-4-server.js
@@ -3,6 +3,8 @@ var server = express();
 var http = require('http').Server(server);
 
 server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+server.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 server.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
